4dd3c722d40374c8d006ec0e085d55d719373b0f
hoor120 <hoorarshad123786@gmail.com>
2025-03-20 10:48:07 +0500

    first commit

---
+++ python_file class work.py
@@ -0,0 +1,244 @@
+# print('hoor')
+
+# 1:-  sum practice
+
+# a=2
+# b=4
+# sum= 2+4
+# print(sum)
+# print(type(a))
+
+
+# 2:-  list practice
+
+# l=[56,46.67,3+0]
+# print(l)
+# print(type(l))
+
+# r=[33+22 ,1-78]
+# print(r)
+
+
+# 3:-  dictionary practice
+ 
+# b = {"name":"noor","age":"24","degree":"bs"}
+# print(b)
+
+# d = {' hall name':'qazafi','higth':'7.8'}
+# print(d)
+
+# x= {'ras': "monal",'noor':" anam", 'weight': "52.9kg"}
+# print("x")
+
+# print(type(x))
+
+# y ={"rass mallai":"bread","apple":"bannana"}
+# print(y)
+# print(type(y))
+
+# t={"hospital name 1": "sara" , "hospital name 2": "ali", "year1": "1998" , "year2" :"1996"}
+# print(t)
+
+
+# 4:-  input type
+
+# a=input("entre your name:")
+# print(a,type(a))
+
+# 5:-  if conditions
+
+# num = 30
+
+# if num == 45 :
+#     print("you are passed")
+# elif num >= 90:
+#         print("you are passed grade at A")
+# else :
+#     print("you are not passed")
+
+# num =20
+# num2 = 40
+# if num ==20:
+#     if num2 == 40:
+#         print("u got both right")
+#     else:
+#         print("u got 2nd wrong")
+# else:
+#     print("u got both wrong")
+
+
+# 6:-  type casting
+
+# num = "45"
+# print(type("num"))
+
+# num2 = 45
+# num2 = str(num2)
+# print(type(num2))
+
+# num3 = "0.23"
+# num3 = float(num3)
+# print(type(num3))
+
+# val = input("enter ur age in num:")
+# val1 = int(val)
+# print(type(val),type(val1))
+
+
+# class assignment  no 1
+
+# edu=int(input("enter your education"))
+# age=int(input("enter your age"))
+# hight=int(input("enter your hight"))
+ 
+# if(edu >= 12 and(age <= 32 and age >= 18)):
+#     print ("passed")
+# elif((age <= 32 and age >= 18 )and hight >= 5.6):
+#     print("passed")
+# elif(hight >= 5.6 and edu >= 12):
+#     print("passed")
+# else:
+#     print("not passed")
+
+# class assigmnt 2
+
+# marks =int(input( "enter ur marks"))
+# total_num = int(input("enter ut total num"))
+# percentage = marks / total_num*100
+# print(percentage)
+
+# class assigment 3
+
+# marks =int(input("marks"))
+# total_num = int(input("total_num"))
+# percentage = marks / total_num*100 
+# print(percentage)
+
+
+
+# 7:-  Multiline String
+
+# b = """heloo world how r 
+#      you """
+     
+# print(b)
+
+# c = "hello world"
+# print(type(c) , len(c))
+
+# 8:-  concatation
+
+# a ='hello'
+# b ='world'
+# print (a +" "+ b)
+
+# a = 42
+# b = 43
+# print(a+b)
+# print(str(a) +  str(b) )
+
+
+# 9:-  format in string and integers / float
+
+# marks = 45
+# total ='your total marks is 100 and you got {}'
+
+# obt = total.format(marks)
+# print(obt)
+
+# 10:-  string silicing
+
+# a = 'hello world'
+# print(a[-3:-1])
+
+
+# 11:-  split in string
+
+# a= 'hello ai'
+# b= a.split(' ')
+# print(a,b)
+
+# 12:-  uper and lower case in string
+
+# st = 'Hello world'
+# up = st.upper()
+# lo = st.lower()
+# print(up, lo)
+
+
+# 13:-  capitalization
+
+# p = 'hoor'
+# q = 'eman' 
+# print(p .capitalize() +' ' + q.capitalize())
+
+# p ='jaranwala' ,'faislabad','lahore'
+
+
+# w='lahore'
+# q='faislabad'
+# s= 'multan'
+
+# print(p.capitalize() +' ' + q.capitalize() + ' ' + w.capitalize())
+
+# p = p.upper()
+# q = q.upper()
+# w = w.upper()
+# print(p,q,w)
+
+# print(w[0:1].upper() + w[1:6])
+# print(q[0:1].upper() + q[1:9])
+
+# print(w[0:1].lower() + w[1:6].upper() , q[0:1].lower() + q[1:9].upper() , s[0:1].lower()+ s[1:6].upper())
+
+
+# 14:-  replace methode
+# t = w.replace('l','L') , q.replace('f','F') , s.replace ('m', 'M')
+# print(t)
+
+
+# 15:-  lists
+
+# ls = ['lahore' , 'multan' ,'karachi']
+# ls = []
+# print(type(ls),ls)
+# print(type(ls),ls)
+
+# ls[1:2]=['Multan']
+# print(ls)
+
+# del ls[0]
+# print(ls)
+
+# ls.clear()
+# print(ls)
+
+# 16:-  for Loops
+
+
+# ls = ['apple','banana','mango',90]
+
+# ls.append(80)
+# ls.insert(3,80)
+# ls.pop(1)                for pop
+# print(ls)
+# for a in ls:
+#     print(a)
+
+#  4:-  class assignment                     reverse methode
+
+ls = ['lahore' , 'karachi', 'multan' , 'jaranwala' , 'islamabad']
+# lp=[]
+# for a in ls:
+#      lp.insert(0,a)
+# print(lp)
+     
+     
+ # 5:-  class assignment                       first letter capital
+ 
+ 
+lp =[]
+for a in ls:
+     ls[0:1].upper() + ls[1:2].upper() + ls[2:3].upper() + ls[3:4].upper + ls[4:].upper
+     print(ls)
+ 
